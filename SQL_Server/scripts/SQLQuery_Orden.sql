c801c68058e8b21c79bce28a901b104dfdc09b7d
DarioMC <dmcsubotai95@gmail.com>
2017-08-27 13:22:29 -0600

    scripts sql server

---
+++ SQL_Server/scripts/SQLQuery_Orden.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Orden] (
+    [IdOrden]     INT      IDENTITY (1, 1) NOT NULL,
+	[IdCarrito]   INT      NOT NULL,
+    [Fecha]       DATETIME NOT NULL,
+	[FechaInicio]       DATETIME NOT NULL,
+	[FechaFinal]       DATETIME NOT NULL,
+    [Usuario]   INT      NOT NULL,
+    [Tarifa] INT      NOT NULL,
+	[Categoria] VARCHAR(20)      NULL,
+	[Descripción] VARCHAR(500)      NOT NULL
+    
+    CONSTRAINT [PK_IdOrden] PRIMARY KEY CLUSTERED ([IdOrden] ASC)
+);
