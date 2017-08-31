7a2511494403bf357aab17c0bf0d74bcc9f82291
Adrian <adriangarro@icloud.com>
2017-08-31 16:19:26 -0600

    Small change in extension

--- ORACLE/B2-tablespaces.sql
+++ ORACLE/B2-tablespaces.sql
@@ -1,9 +1,9 @@
 CREATE TABLESPACE grupoAdrianDarioOscarDaniel_tbl
-    DATAFILE 'tbs_grupo.dat'
+    DATAFILE 'tbs_grupo.dbf'
         SIZE 8K
         AUTOEXTEND OFF;
     
 CREATE TABLESPACE grupoAdrianDarioOscarDaniel_idx
-    DATAFILE 'idx_grupo.dat'
+    DATAFILE 'idx_grupo.dbf'
     SIZE 8K
     AUTOEXTEND OFF;
