6d992a8607bcc375c0ede7fc31fe8a642f6ee072
Adrian <adriangarro@icloud.com>
2017-08-31 16:14:34 -0600

    ORACLE - Adding part B2.

---
+++ ORACLE/B2-tablespaces.sql
@@ -0,0 +1,9 @@
+CREATE TABLESPACE grupoAdrianDarioOscarDaniel_tbl
+    DATAFILE 'tbs_grupo.dat'
+        SIZE 8K
+        AUTOEXTEND OFF;
+    
+CREATE TABLESPACE grupoAdrianDarioOscarDaniel_idx
+    DATAFILE 'idx_grupo.dat'
+    SIZE 8K
+    AUTOEXTEND OFF;
