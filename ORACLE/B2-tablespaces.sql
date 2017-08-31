a851c141bc7034632152bd0957131014e56875e5
Adrian <adriangarro@icloud.com>
2017-08-31 16:21:01 -0600

    Adding user

--- ORACLE/B2-tablespaces.sql
+++ ORACLE/B2-tablespaces.sql
@@ -7,3 +7,7 @@ CREATE TABLESPACE grupoAdrianDarioOscarDaniel_idx
     DATAFILE 'idx_grupo.dbf'
     SIZE 8K
     AUTOEXTEND OFF;
+    
+CREATE USER egarro IDENTIFIED BY abcd123 DEFAULT TABLESPACE grupoAdrianDarioOscarDaniel_tbl;
+GRANT dba, connect, resource TO egarro;
+GRANT CREATE ANY VIEW TO egarro WITH ADMIN OPTION;
