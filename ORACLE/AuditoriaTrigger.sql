2b93e942feae2cc292395e8472625268f990edca
Daniel Solís <jodaniel94@gmail.com>
2017-09-11 11:53:00 -0600

    Arreglos parte mia

---
+++ ORACLE/AuditoriaTrigger.sql
@@ -0,0 +1,21 @@
+--Tabla Auditoria necesaria para llevar un orden en los cambios al salario.
+
+CREATE TABLE AUDITORIA(FECHA_CAMBIO DATE NOT NULL,
+  USUARIO VARCHAR2(25) NOT NULL, 
+  SALARIO_OLD INT NOT NULL, 
+  SALARIO_NEW INT NOT NULL);
+  
+
+--Trigger Auditoria, inserta como tipo log, todos los cambios que se hagan al salario.
+
+CREATE OR REPLACE TRIGGER AuditoriaTrigger BEFORE UPDATE
+  ON EMPLEADO FOR EACH ROW
+BEGIN
+  IF UPDATING(SALARIO) THEN
+    INSERT INTO AUDITORIA
+    VALUES(SYSDATE, USER, :old.SALARIO, :new.SALARIO);
+  ELSIF UPDATING THEN
+    INSERT INTO AUDITORIA
+    VALUES(SYSDATE, USER, :old.key, :new.key);
+  END IF;
+END AuditoriaTrigger;
---
+++ ORACLE/SalarioPositivoTrigger.sql
@@ -0,0 +1,11 @@
+--Trigger que se activa al insertar o modificar el salario de un empleado.
+
+CREATE OR REPLACE TRIGGER SalarioPositivo
+  BEFORE INSERT OR UPDATE OF SALARIO
+  ON EMPLEADO
+  FOR EACH ROW
+BEGIN
+  IF :new.SALARIO < 0 THEN
+    RAISE_APPLICATION_ERROR(-20100, 'Por favor inserte un valor positivo');
+  END IF;
+END SalarioPositivo;
