64401e9772dd4385ee820d04cd60567c30d6d7de
ochavarria <ocachl4@hotmail.com>
2017-09-13 22:39:16 -0600

    Login

---
+++ Login/Login-Neo4j/Login-Neo4j/Controllers/RegistrationController.cs
@@ -0,0 +1,38 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_Neo4j.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register(Login_Neo4j.Models.User Userlog)
+        {
+            using (var driver = GraphDatabase.Driver("bolt://127.0.0.1:7687", AuthTokens.Basic("neo4j", "chavacampos14")))
+            using (var session = driver.Session())
+            {
+                //var result = session.Run("CREATE (n) RETURN n");
+                var result = session.Run("create(x: Persona{" +
+                    "Nombre: '" + Userlog.Nombre + "'," +
+                    "Apellido:'" + Userlog.Apellidos + "'," +
+                    "Correo: '" + Userlog.Correo + "'," +
+                    "Usuario: '" + Userlog.UserName + "'," +
+                    "Contrasena:'" + Userlog.Password + "'," +
+                    "Telefono:'" + Userlog.Telefono + "'," +
+                    "Genero:'" + Userlog.Genero + "'," +
+                    "Cedula:'" + Userlog.Cedula + "'," +
+                    "FechaN: '" + Userlog.FechaN + "'}) return x");
+
+                    return View("Index");
+            }
+        }
+    }
+}
---
+++ Login/Login-Neo4j/Login-Neo4j/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login_Neo4j.Models
+{
+    public partial class User
+    {
+        public String Nombre { get; set; }
+        
+        public String Apellidos { get; set; }
+
+        [DisplayName(displayName: "Usuario")]
+        [Required(ErrorMessage ="Campo Vacio ")]
+        public String UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Campo Vacio")]
+        public String Password { get; set; }
+
+        public int Telefono { get; set; }
+
+        public String Correo { get; set; }
+        
+        public String Genero { get; set; }
+
+        public int Cedula { get; set; }
+
+        public DateTime FechaN { get; set; }
+
+
+
+        
+    }
+}
