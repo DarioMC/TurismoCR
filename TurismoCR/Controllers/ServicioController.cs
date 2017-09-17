cca4f088160751ea3f58cba6dc93a56c586ea2c9
DarioMC <dmcsubotai95@gmail.com>
2017-09-16 19:35:29 -0600

    ServicioController agregado

---
+++ TurismoCR/Controllers/ServicioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace TurismoCR.Controllers
+{
+    public class ServicioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void ConexionMongoDB()
+        {
+            
+            /// Al crear la base mongo para esta conexión  nombrarla --> TurismoCR 
+            var mc = new MongoClient(connectionString: "mongodb://localhost");
+            var db = mc.GetDatabase("TurismoCR");
+
+            ///MongoDatabase mongo = mc.GetDatabase("TurismoCR");
+        }
+
+    }
+}
