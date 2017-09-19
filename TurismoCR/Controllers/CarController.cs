79629b1553c46a56d360d71be6ea85c7bd1567b7
jodaniel <jodaniel94@gmail.com>
2017-09-18 22:05:07 -0600

    Conexiones a Redis y SQL Server

---
+++ TurismoCR/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Configuration;
+
+namespace TurismoCR.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+            IDatabase redisDB = redis.GetDatabase();
+
+            redisDB.StringSet("Test", "123queso");
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ TurismoCR/Data/TurismoCRContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TurismoCR.Models;
+
+namespace TurismoCR.Data
+{
+    public class TurismoCRContext : DbContext
+    {
+        public TurismoCRContext(DbContextOptions<TurismoCRContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<Reseña> Reseñas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Orden>().ToTable("Orden");
+            modelBuilder.Entity<Reseña>().ToTable("Resenia");
+        }
+    }
+}
