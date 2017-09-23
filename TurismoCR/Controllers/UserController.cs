cea3a9de1d050a7691f0360e224b6e4f0b578638
Adrian <adriangarro@icloud.com>
2017-09-23 15:19:35 -0600

    Agregando alertas a login

--- TurismoCR/Controllers/UserController.cs
+++ TurismoCR/Controllers/UserController.cs
@@ -21,37 +21,47 @@ namespace TurismoCR.Controllers
 
         [HttpPost]
 		public ActionResult Auth(User user) {
-			var client = new GraphClient(
-                // cambiar password (adrian) por el de su base Neo4j
-                new Uri("http://localhost:7474/db/data"), "neo4j", "adrian"
-            );
-			client.Connect();
-            var userConsulted = client
-                .Cypher
-                .Match("(userNeo4j:User)")
-                .Where((User userNeo4j) => userNeo4j.UserName == user.UserName)
-                .Return(userNeo4j => userNeo4j.As<User>())
-                .Results;
-            if (userConsulted.Any()) {
-                var foundUser = userConsulted.First();
-                if (foundUser.Password == user.Password) {
-                    Response.Cookies.Append("openSession",
-                        foundUser.UserName,
-                        new CookieOptions
-                        {
-                            Expires = DateTimeOffset.Now.AddHours(1)
-                        }
-                    );
-                    // TODO Admin hide car with cookie config
-				}
+            try {
+				var client = new GraphClient(
+				    // cambiar password (adrian) por el de su base Neo4j
+				    new Uri("http://localhost:7474/db/data"), "neo4j", "adrian"
+			    );
+				client.Connect();
+				var userConsulted = client
+					.Cypher
+					.Match("(userNeo4j:User)")
+					.Where((User userNeo4j) => userNeo4j.UserName == user.UserName)
+					.Return(userNeo4j => userNeo4j.As<User>())
+					.Results;
+				if (userConsulted.Any()) {
+					var foundUser = userConsulted.First();
+					if (foundUser.Password == user.Password) {
+						Response.Cookies.Append("openSession",
+							foundUser.UserName,
+							new CookieOptions
+							{
+								Expires = DateTimeOffset.Now.AddHours(1)
+							}
+						);
+						TempData["msg"] = "<script>alert('Excelente, tu usuario ha sido identificado.');</script>";
+						// TODO Admin hide car with cookie config
+					} else {
+                        TempData["msg"] = "<script>alert('Contraseña incorrecta.');</script>";
+                    }
+                } else {
+                    TempData["msg"] = "<script>alert('No existe el usuario en el sistema.');</script>";
+                }
+            }
+            catch {
+                TempData["msg"] = "<script>alert('No hay comunicación con Neo4j.');</script>";
             }
             return RedirectToAction("Index", "Home");
-
 		}
 
         public ActionResult LogOut() {
             if (Request.Cookies["openSession"] != null) {
                 Response.Cookies.Delete("openSession");
+
 			}
             return RedirectToAction("Index", "Home");
         }
