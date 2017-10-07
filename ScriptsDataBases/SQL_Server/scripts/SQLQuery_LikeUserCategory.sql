4bb153b8abf7303849cd67e1c1248d6b75e341eb
DarioMC <dmcsubotai95@gmail.com>
2017-10-06 21:51:35 -0600

    script para registrar likes de los clientes

---
+++ ScriptsDataBases/SQL_Server/scripts/SQLQuery_LikeUserCategory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[LikeUsuarioCategoria] (
+    [IdLikeUserCategoria]    INT           IDENTITY (1, 1) NOT NULL,
+    [Categoria]   VARCHAR (20)  NOT NULL,
+    [Usuario]      VARCHAR (20)  NOT NULL,
+    CONSTRAINT [PK_IdLikeUsuarioCategoria] PRIMARY KEY CLUSTERED ([IdLikeUserCategoria] ASC)
+);
